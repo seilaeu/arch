379139aa8f1adb96682e3bcab3c3b3a76d6d86c4
seilaeu <ribeiro.jose.a.n@gmail.com>
2018-03-22 18:01:29 +0000

    add new files and folders

--- scripts/install-p0.sh
+++ scripts/install-p0.sh
@@ -8,21 +8,20 @@ loadkeys pt-latin9
 # Verificar o valor das variáveis vg, lv e xy. 
 
 # Volume group name
-vg=pclvm
+read -p 'Volume group name: ' volumegroupnamevar
 
 # Logical volume name
-lv=arch
-
-# Device's path
-device=/dev/$vg/$lv
+read -p 'Logical volume name: ' logicalvolumenamevar
 
 # Logical volume size in Megabites or Gygabites
-xy=20G#/bin/bash
-# Teclado
+read -p 'Logical volume size in GB: ' logicalvolumesizevar
 
-lvcreate -L$xy -n $lv $vg
+# Device's path
+device=/dev/$volumegroupnamevar/$logicalvolumenamevar
 
-mkfs.btrfs -f -L "$lv" /dev/$vg/$lv
+lvcreate -L"$logicalvolumesizevar"G -n $logicalvolumenamevar $volumegroupnamevar
+
+mkfs.btrfs -f -L "$logicalvolumenamevar" /dev/$volumegroupnamevar/$logicalvolumenamevar
 
 # Criar directorias (activo e reserva) e subvolume raiz
 
@@ -30,20 +29,16 @@ mkdir -p /media/arch
 
 ##Mudar sdaX para a partição alvo
 
-mount -o rw,suid,dev,exec,auto,nouser,async,noatime,space_cache,autodefrag,compress=lzo /dev/$vg/$lv /media/arch
+mount -o rw,suid,dev,exec,auto,nouser,async,noatime,space_cache,autodefrag,compress=lzo $device /media/arch
 mkdir -p /media/arch/__activo
 mkdir -p /media/arch/__reserva
 btrfs sub create /media/arch/__activo/raiz
+btrfs sub set-default /media/arch/__activo/raiz
 sleep 3
 umount -l /media/arch
 sleep 7
 
-# EXT4
-
-#mkfs.ext4 /dev/sdaX &&
-#e2label /dev/sdaX arch &&
-
-#blkid &&
+blkid 
 
 # Montar raiz em /mnt
 
@@ -52,7 +47,7 @@ if [[ ! -e /mnt ]]; then
 elif [[ ! -d /mnt ]]; then
     echo "/mnt already exists but is not a directory" 1>&2
 fi
-mount /dev/$vg/$lv /mnt
+mount -o subvol=__activo/raiz /dev/$volumegroupnamevar/$logicalvolumenamevar /mnt
 df -h &&
 sleep 7
 
