aec8772b1315e8b17201d448c40eedaea09e9fc6
seilaeu <ribeiro.jose.a.n@gmail.com>
2018-10-03 20:04:38 +0100

    Update i5-plasma.sh

--- scripts/i5-plasma.sh
+++ scripts/i5-plasma.sh
@@ -1,4 +1,4 @@
 # Plasma 5
 
-pacman -S yaourt --needed 
-pacman -S plasma-desktop dolphin kmymoney kcalc k3b okular digikam gwenview qbittorrent ark sddm konsole kate clementine kdeconnect spectacle kfind --needed
+pacman -S plasma-desktop konsole dolphin kmymoney kate kcalc okular gwenview qbittorrent ark sddm clementine kdeconnect spectacle \
+kfind --needed
