b8f3f4ef0507072cfb0fe68e65f639f82ce4a2c1
seilaeu <joseribeiro@protonmail.com>
2020-10-26 23:02:03 +0000

    Update i5-plasma.sh

--- scripts/i5-plasma.sh
+++ scripts/i5-plasma.sh
@@ -1,6 +1,6 @@
 # Plasma 5
 
-pacman -S plasma-desktop konsole dolphin kate kcalc okular gwenview qbittorrent ark clementine \
-kdeconnect spectacle kfind breeze-gtk breeze-kde4 kde-gtk-config sddm --needed
+pacman -S plasma-desktop konsole dolphin kate kcalc okular gwenview qbittorrent ark clementine kinfocenter kipi-plugins redshift \
+kdeconnect kdeplasma-addons plasma-nm plasma-pa spectacle kfind breeze-gtk breeze-kde4 kde-gtk-config sddm --needed
 
 systemctl enable sddm
