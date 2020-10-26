a2093b8602e754802ec85b56a6337ed94520bac5
seilaeu <joseribeiro@protonmail.com>
2020-10-26 20:07:48 +0000

    Update i1-preparation.sh

--- scripts/i1-preparation.sh
+++ scripts/i1-preparation.sh
@@ -13,9 +13,15 @@ ip link
 # Partição UEFI
 mkfs.fat -F32 /dev/sda1
 
-# Criar partição raiz e montá-la em /mnt
+# Criar partição root e home e montá-las em /mnt e em /mnt/home, respectivamente:
 
-mkfs.ext4 /dev/sdaX
-mount /dev/sdaX /mnt
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+
+mount /dev/sda2 /mnt
+
+mkdir /mnt/home
+mount /dev/sda3 /mnt/home
 
 lsblk
+
