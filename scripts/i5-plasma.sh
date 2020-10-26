c49f8fd15a4b9f7329667fe44e2684f6251d4590
seilaeu <joseribeiro@protonmail.com>
2020-10-26 23:43:53 +0000

    Update i5-plasma.sh

--- scripts/i5-plasma.sh
+++ scripts/i5-plasma.sh
@@ -4,3 +4,14 @@ pacman -S plasma-desktop konsole dolphin kate kcalc okular gwenview qbittorrent 
 kdeconnect kdeplasma-addons plasma-nm plasma-pa spectacle kfind breeze-gtk breeze-kde4 kde-gtk-config sddm --needed
 
 systemctl enable sddm
+
+cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
+
+echo "Definir o tema breeze"
+
+sleep 5
+
+nano /etc/sddm.conf
+
+
+
