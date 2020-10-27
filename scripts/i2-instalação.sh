1597e2226827185c63c2097bf6dbaffcc2805343
seilaeu <joseribeiro@protonmail.com>
2020-10-27 18:27:00 +0000

    Update and rename i2-base.sh to i2-instalação.sh

---
+++ scripts/i2-instalação.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# Mirrors
+
+pacman -Syy
+pacman -S reflector --needed 
+reflector --verbose --country 'Portugal' --sort rate --save /etc/pacman.d/mirrorlist 
+pacman -Syy
+pacman -Su
+
+# Instalar sistema base
+pacstrap -i /mnt/ base linux linux-firmware sudo nano intel-ucode
+
+# fstab
+
+genfstab -U -p /mnt >> /mnt/etc/fstab 
+cat /etc/fstab
+
+# Mudar raiz para a directoria mnt
+
+arch-chroot /mnt /bin/bash
