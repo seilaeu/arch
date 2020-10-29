9aaea14029ab2a13bf45921db5cda403e5293373
seilaeu <joseribeiro@protonmail.com>
2020-10-29 23:16:30 +0000

    Update i1-preparation.sh

--- scripts/i1-preparation.sh
+++ scripts/i1-preparation.sh
@@ -1,39 +1,35 @@
 #/bin/bash
 
-# Teclado português
+# Data e hora
 
-loadkeys pt-latin9 
-
-# Modo UEFI
-
-ls /sys/firmware/efi/efivars
-
-
-# Interface de rede
-
-ip link
-
-# Ligação à Internet
-
-ping -c 3 archlinux.org
-
-# Relógio
+timedatectl set-timezone Europe/Lisbon
 
 timedatectl set-ntp true
 
+date
+
+sleep 5
+
+
 # Partição UEFI
-mkfs.fat -F32 /dev/sda1
+
+#mkfs.fat -F32 /dev/sda1
+
 
 # Partição root 
 
-mkfs.ext4 /dev/sda2
-mount /dev/sda2 /mnt
+#mkfs.ext4 /dev/sda2
+
+#mount /dev/sda2 /mnt
+
 
 # Partição home 
 
-mkfs.ext4 /dev/sda3
-mkdir /mnt/home
-mount /dev/sda3 /mnt/home
+#mkfs.ext4 /dev/sda3
+
+#mkdir /mnt/home
+
+#mount /dev/sda3 /mnt/home
 
 lsblk
 
