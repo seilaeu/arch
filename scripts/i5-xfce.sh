170071503c10aafd0f9b029d95301cd13de16297
seilaeu <joseribeiro@protonmail.com>
2020-10-29 23:23:49 +0000

    Delete i5-xfce.sh

--- scripts/i5-xfce.sh
+++
@@ -1,9 +0,0 @@
-pacman -S yaourt --needed
-
-pacman -S xfce4 xfce4-whiskermenu-plugin xfce4-taskmanager thunar-archive-plugin ristretto mousepad atril galculator gvfs xfce4-pulseaudio-plugin menulibre --needed
-yaourt -S engrampa-thunar --needed
-yaourt -S mugshot --needed 
-yaourt -S faenza-cupertino-icon-theme
-gpg --keyserver http://pgp.mit.edu:11371 --recv-keys CDBD406AA1AA7A1D &&
-yaourt -S numix-frost-themes 
-
