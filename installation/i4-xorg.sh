a35ed6863c55b6a31bf391f9e60a4224d8a44509
seilaeu <joseribeiro@protonmail.com>
2020-10-30 18:31:02 +0000

    Update i4-xorg.sh

--- installation/i4-xorg.sh
+++ installation/i4-xorg.sh
@@ -1,4 +1,5 @@
 # Servidor gráfico
 
-pacman -S xorg-server xorg-xinit xorg-xkill xdpyinfo
+pacman -S xorg-server xorg-xinit xorg-xkill xdpyinfo xorg-font-util xorg-fonts-100dpi xorg-fonts-75dpi xorg-mkfontscale
+
 
